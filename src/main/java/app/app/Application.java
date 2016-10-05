1c51a740a59082b4ff0da6e4af1e4e26d28b7ab9
Dimitrios Desillas <ddesillas@yahoo.gr>
2016-10-05 18:37:14 +0300

    Gert Post Params

--- src/main/java/app/app/Application.java
+++ src/main/java/app/app/Application.java
@@ -1,6 +1,8 @@
 package app.app;
 
 import app.app.http.MyHttpHandler;
+import app.app.http.ParameterFilter;
+import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -16,7 +18,10 @@ public class Application
         try
         {
             HttpServer server = HttpServer.create(new InetSocketAddress(8082),0);
-            server.createContext("/checker", new MyHttpHandler());
+
+            HttpContext context=server.createContext("/checker", new MyHttpHandler());
+            context.getFilters().add(new ParameterFilter());
+
             server.setExecutor(null); // creates a default executor
             server.start();
         }
--- src/main/java/app/app/http/MyHttpHandler.java
+++ src/main/java/app/app/http/MyHttpHandler.java
@@ -6,20 +6,35 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Map;
+
 
 /**
  * Created by pcmagas on 5/10/2016.
  */
 public class MyHttpHandler implements HttpHandler
 {
+//    static String convertStreamToString(java.io.InputStream is) {
+//        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+//        return s.hasNext() ? s.next() : "";
+//    }
+
     public void handle(HttpExchange t) throws IOException
     {
-        InputStream is = t.getRequestBody();
-//        read(is); // .. read the request body
-        String response = "This is the response";
-        t.sendResponseHeaders(200, response.length());
+        Map<String, Object> params = (Map<String, Object>)t.getAttribute("post_parameters");
+
+        Object[] paramsAsArray=params.values().toArray();
+        String s="";
+
+        for(Object o : paramsAsArray)
+        {
+            s+=(String)o;
+        }
+
+        t.sendResponseHeaders(200, s.length());
+
         OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
+        os.write(s.getBytes());
         os.close();
     }
 }
